8bbf8da716672d1dab21e232fc0e9b3ddc92e0ec
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-08-01 18:10:55 -0300

    criado a controler

---
+++ src/main/java/dev/java10X/CadastroDeNinjas/Controller.java
@@ -0,0 +1,14 @@
+package dev.java10X.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boas-vindas")
+    public String boasVindas() {
+        return "Boas Vindas";
+    }
+}
