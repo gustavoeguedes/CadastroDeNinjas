1c077148bfea79ab788a294d74666e1f90d7b6b4
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-08-04 19:06:21 -0300

    feat: add method put

--- src/main/java/dev/java10X/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10X/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -34,9 +34,9 @@ public class NinjaController {
         return ninjaService.findById(id);
     }
 
-    @PutMapping("alterar-id")
-    public String alterarId() {
-        return "Alterar Id";
+    @PutMapping("alterar/{id}")
+    public NinjaModel alterarId(@PathVariable Long id, @RequestBody NinjaModel ninjaAtualizado) {
+        return ninjaService.atualizarNinja(id, ninjaAtualizado);
     }
 
     @DeleteMapping("deletar/{id}")
--- src/main/java/dev/java10X/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/dev/java10X/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -34,6 +34,14 @@ public class NinjaService {
          ninjaRepository.deleteById(id);
     }
 
+    public NinjaModel atualizarNinja(Long id, NinjaModel ninjaAtualizado) {
+        if(ninjaRepository.existsById(id)) {
+            ninjaAtualizado.setId(id);
+            return ninjaRepository.save(ninjaAtualizado);
+        }
+        return null;
+    }
+
 
 
 }
