30480feda9cf8adc0e8bcf52b8b0d2de09d1c9fd
gustavoeguedes <gustavoestevesguedes12@gmail.com>
2025-08-05 12:50:08 -0300

    chore: include dto and mapper to ninja

--- src/main/java/dev/java10X/CadastroDeNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/java10X/CadastroDeNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,28 @@
 package dev.java10X.CadastroDeNinjas.Ninjas;
 
+import dev.java10X.CadastroDeNinjas.Missoes.MissoesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private Long id;
+
+    private String nome;
+
+
+    private String email;
+
+    private int idade;
+
+
+    private String imageUrl;
+
+    private MissoesModel missoes;
+    private String rank;
+
 }
